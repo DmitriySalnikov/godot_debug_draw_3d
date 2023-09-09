f1a39658bc3f9c26c887623793576d4843151c98
DmitriySalnikov <salnikov.mine@yandex.ru>
2023-09-10 02:52:39 +0300

    switched to using `editor` instead of `template_debug`

--- dev_build_godot_cpp.bat
+++ dev_build_godot_cpp.bat
@@ -13,7 +13,7 @@ git apply --ignore-space-change --ignore-whitespace ../patches/unity_build.patch
 ::if errorlevel 1 ( echo Failed to generate and compile debug godot-cpp source code. Code: %errorlevel% && exit /b %errorlevel% )
 
 title win x64 debug dev
-scons platform=windows target=template_debug arch=x86_64 dev_build=yes debug_symbols=yes %api% generate_bindings=yes
+scons platform=windows target=editor arch=x86_64 dev_build=yes debug_symbols=yes %api% generate_bindings=yes
 if errorlevel 1 ( echo Failed to generate and compile debug godot-cpp source code. Code: %errorlevel% && exit /b %errorlevel% )
 
 ::title win x64 editor
@@ -21,7 +21,7 @@ if errorlevel 1 ( echo Failed to generate and compile debug godot-cpp source cod
 ::if errorlevel 1 ( echo Failed to generate and compile debug godot-cpp source code. Code: %errorlevel% && exit /b %errorlevel% )
 
 title win x64 debug
-scons platform=windows target=template_debug arch=x86_64 debug_symbols=yes %api%
+scons platform=windows target=editor arch=x86_64 debug_symbols=yes %api%
 if errorlevel 1 ( echo Failed to generate and compile debug godot-cpp source code. Code: %errorlevel% && exit /b %errorlevel% )
 
 title win x64
