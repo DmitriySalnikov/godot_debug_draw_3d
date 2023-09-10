61f9154ea269c84769f6e31f6dd9a54a02922cac
DmitriySalnikov <salnikov.mine@yandex.ru>
2023-09-11 01:58:23 +0300

    restored string debugging patch

--- dev_build_godot_cpp.bat
+++ dev_build_godot_cpp.bat
@@ -6,19 +6,11 @@ git apply --ignore-space-change --ignore-whitespace ../patches/always_build_fix.
 git apply --ignore-space-change --ignore-whitespace ../patches/godot_cpp_exclude_unused_classes.patch
 git apply --ignore-space-change --ignore-whitespace ../patches/1165.patch
 git apply --ignore-space-change --ignore-whitespace ../patches/unity_build.patch
-::git apply --ignore-space-change --ignore-whitespace ../patches/debug_string.patch
-
-::title win x64 editor dev
-::scons platform=windows target=editor arch=x86_64 dev_build=yes %api% generate_bindings=yes
-::if errorlevel 1 ( echo Failed to generate and compile debug godot-cpp source code. Code: %errorlevel% && exit /b %errorlevel% )
+git apply --ignore-space-change --ignore-whitespace ../patches/debug_string.patch
 
 title win x64 debug dev
-scons platform=windows target=editor arch=x86_64 dev_build=yes debug_symbols=yes %api% generate_bindings=yes
+scons platform=windows target=editor arch=x86_64 dev_build=yes debug_symbols=yes generate_bindings=yes %api%
 if errorlevel 1 ( echo Failed to generate and compile debug godot-cpp source code. Code: %errorlevel% && exit /b %errorlevel% )
-
-::title win x64 editor
-::scons platform=windows target=editor arch=x86_64 debug_symbols=yes %api% generate_bindings=yes
-::if errorlevel 1 ( echo Failed to generate and compile debug godot-cpp source code. Code: %errorlevel% && exit /b %errorlevel% )
 
 title win x64 debug
 scons platform=windows target=editor arch=x86_64 debug_symbols=yes %api%
